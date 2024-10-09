39c9aef40b1846c213723e45b34149ae29fa781f
yuanrang.peng@tuya.com <yuanrang.peng@tuya.com>
2024-10-09 18:30:15 +0800

    fs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xx1124961758/gogogo
+
+go 1.22.5
---
+++ mypackage.go
@@ -0,0 +1,8 @@
+package mypackage
+
+import "fmt"
+
+// SayHello 输出问候语
+func SayHello(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
